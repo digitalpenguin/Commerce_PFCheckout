a875f7d82a7c092fa7b3eae18df6ef2bdd0ced30
digitalpenguin <murray@digitalpenguin.hk>
2021-03-30 15:50:09 +0800

    Make sure field for logging is state

--- core/components/commerce_pfcheckout/src/Gateways/PostFinanceGateway.php
+++ core/components/commerce_pfcheckout/src/Gateways/PostFinanceGateway.php
@@ -144,9 +144,9 @@ class PostFinanceGateway implements GatewayInterface {
             $data = json_decode($pfTransaction, true);
 
             // Check if authorized - ( AUTHORIZED, FAILED )
-            $transaction->log('Payment Status is: ' . $data['status'] . '. ' . print_r($data, true),comTransactionLog::SOURCE_GATEWAY);
+            $transaction->log('Payment Status is: ' . $data['state'] . '. ' . print_r($data, true),comTransactionLog::SOURCE_GATEWAY);
             $transaction->save();
-            $this->commerce->modx->log(MODX_LOG_LEVEL_DEBUG, 'Payment Status is: ' . $data['status']);
+            $this->commerce->modx->log(MODX_LOG_LEVEL_DEBUG, 'Payment Status is: ' . $data['state']);
             $this->commerce->modx->log(MODX_LOG_LEVEL_DEBUG, print_r($data, true));
         }
         return new \DigitalPenguin\Commerce_PFCheckout\Gateways\Transactions\Order($order,$data);
