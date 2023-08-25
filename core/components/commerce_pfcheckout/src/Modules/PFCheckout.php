cfe21c575a651f16e5155c327d9f4d9d1ea586a1
Murray Wood <murray@digitalpenguin.studio>
2023-08-25 12:20:24 +0800

    Use Commerce EventDispatcher wrapper

--- core/components/commerce_pfcheckout/src/Modules/PFCheckout.php
+++ core/components/commerce_pfcheckout/src/Modules/PFCheckout.php
@@ -6,7 +6,7 @@ use modmore\Commerce\Admin\Sections\SimpleSection;
 use modmore\Commerce\Events\Admin\PageEvent;
 use modmore\Commerce\Modules\BaseModule;
 use modmore\Commerce\Events\Gateways;
-use Symfony\Component\EventDispatcher\EventDispatcher;
+use modmore\Commerce\Dispatcher\EventDispatcher;
 
 require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
 
